087deae4f32bd4e42de94317ad321f242a04ab2a
mmtolbert <mmtolbert@cox.net>
2018-03-03 17:09:16 -0500

    Generates Random Characters and Returns Password

--- randomPassGen.c
+++ randomPassGen.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+char getRandChar(int capitols, int specials);
 
 /**
  * This takes in the arguments from the command line and interprets them,
@@ -13,6 +16,8 @@ int main(int argc, char** argv) {
   int numChars; // Integer value representing the desired length of the users pass
   int dataType; // value 1 means user specified capitol, else 0
   int specialChars; // value 1 means user specified special characters
+
+  srand(time(0));
 
   // check for proper formatting of call to function
   if(argc < 2 || argc > 4) { // Make sure the user calls the function with parameters
@@ -34,29 +39,40 @@ int main(int argc, char** argv) {
       printf("Second Argument must be 'Y' or 'N'\n");
       return EXIT_FAILURE;
     } else {
-      dataYpe = 1;
+      dataType = 1;
     }
     // check for special chars specification
     if(argc == 4) {
-      specialChars == *argv[3]; // if specified set special chars to 3 arg
       // check arg validity
-      if(!(specialChars == 'Y' || specialChars == 'N')) {
+      if(!(*argv[3] == 'Y' || *argv[3] == 'N')) {
         printf("Third Argument must be 'Y' or 'N'\n");
         return EXIT_FAILURE;
+      } else {
+        specialChars = 1;
       }
     }
   }
   // if not specified set to default of no
   else {
-    dataType = 'N'; // default N
-    specialChars = 'N'; // default N
+    dataType = 0; // default N
+    specialChars = 0; // default N
   }
 
+  // create an array of character to represent the password
+  char password[numChars]; // password to be set, allocate the size
   // Loop through the specified number of characters specified by the user
   // Call generate randChar
   for(int i = 0; i < numChars; i ++) {
-
+    // here we will call the function to return a random char and add to string
+    password[i] = getRandChar(dataType, specialChars); // set each index to separate call
+    // pass in capitols and special characters
   }
+
+  printf("Your randomly generated password is: \n");
+  for(int i = 0; i < numChars; i++) {
+    printf("%c", password[i]);
+  }
+  printf("\n");
 
   return EXIT_SUCCESS;
 }
@@ -68,6 +84,36 @@ int main(int argc, char** argv) {
  * @param specials Used if user specifies that they want special chars (int = 1)
  * @return Randomly generated character that meets specification
 */
-char* getRandChar(int capitols, int specials) {
+char getRandChar(int capitols, int specials) {
+  // check to see if user specified using captiol and special letters
+  if(capitols == 1 && specials == 1) {
+    // use a switch to decide to randomly decide what to do
+    int whatToDo = rand() % 3; // generated a nuber between 1 and 3 for reference in switch
+    int randSpecial = rand() % 4; // do calculation now so we can skip " character if needed
+    switch(whatToDo) {
+      case 0: // use a captiol
+              return 'A' + (rand() % 26);
+      case 1: // use a special char
+              if(randSpecial == 0) { // make sure we don't return " character
+                return '!'; // default case
+              }
+              else if (randSpecial != 0) {
+                return '!' + randSpecial + 1; // skip " character
+              }
+      case 2: // use neither
+              return 'a' + (rand() % 26);
+    }
+  }
+  // only specials specified
+  else if(capitols == 0 && specials == 1) {
 
+  }
+  // only capitols specified
+  else if(capitols == 1 && specials == 0) {
+
+  }
+  // nothing specified
+  else {
+    return 'a' + (rand() % 26);
+  }
 }
